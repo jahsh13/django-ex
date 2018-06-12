aca85df58eaa0064a01c512b93ed62759d1be193
Jahsh Parriott <jahshua.parriott@leidos.com>
2018-06-11 19:56:46 -0500

    Adding debugging statements

--- welcome/tests.py
+++ welcome/tests.py
@@ -8,20 +8,26 @@ from django.test import TestCase
 # and OpenShift when building an application image.
 class PageViewModelTest(TestCase):
     def test_viewpage_model(self):
+        print("Entering test_viewpage_model...")
         pageview = PageView.objects.create(hostname='localhost')
         pagetest = PageView.objects.get(hostname='localhost')
         self.assertEqual(pagetest.hostname, 'localhost')
+        print("Leaving test_viewpage_model.  hostname = ",pagetest.hostname)
 
 class PageViewTest(TestCase):
     def test_index(self):
+        print("Entering test_index...")
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
+        print("Leaving test_index. status_code = ", resp.status_code)
 
 class DbEngine(TestCase):
     def setUp(self):
         os.environ['ENGINE'] = 'SQLite'
 
     def test_engine_setup(self):
+        print("Entering test_engine_setup...")
         settings = info()
         self.assertEqual(settings['engine'], 'SQLite')
         self.assertEqual(settings['is_sqlite'], True)
+        print("Leaving test_engine_setup.  settings = ", settings)
